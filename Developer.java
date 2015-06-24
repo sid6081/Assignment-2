1430557c1eb1e09a862ab47c7f1f24a1de7a7bab
Siddharth Karandikar <hksiddharthk@gmail.com>
2015-06-24 15:09:36 +0530

    Update Developer.java

--- Developer.java
+++ Developer.java
@@ -1,30 +1,4 @@
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
-
-
-/**
- * @author impadmin
- * This class compares objects of Developer class
- */
-class Comp implements Comparator<Developer>
-{
-	int temp=0;
-	Comp(int x)
-	{
-		temp=x;
-	}
-	@Override
-	public int compare(Developer a, Developer b) {
-		// TODO Auto-generated method stub
-		if(temp==0)
-			return (a.name).compareToIgnoreCase(b.name);
-		else
-			return a.age.compareTo(b.age);
-	}
-}
+import java.util.*;
 
 /**
  * @author impadmin
@@ -36,22 +10,17 @@ class Developer implements EmployeeInterface
 	Scanner sc=new Scanner(System.in);
 	String name;
 	Integer age;
-	static int dev_count=0;
-	static List<String> listName=new LinkedList<String>();
-	static List<Integer> listAge=new LinkedList<Integer>();
+	static int devCount=0;
 	static List<Developer> listObject=new LinkedList<Developer>();
-	
 	@Override
 	public void insert(String a, int b) {
 		// TODO Auto-generated method stub
 		name=a.toLowerCase();
 		age=b;
-		listName.add(name);
-		listAge.add(age);
 	}
 
 	@Override
-	public void sort_name() {
+	public void sortName() {
 		// TODO Auto-generated method stub
 		int temp=0;
 	
@@ -69,7 +38,7 @@ class Developer implements EmployeeInterface
 	}
 
 	@Override
-	public void sort_age() {
+	public void sortAge() {
 		// TODO Auto-generated method stub
 		int temp=0;
 		Collections.sort(listObject, new Comp(1));
